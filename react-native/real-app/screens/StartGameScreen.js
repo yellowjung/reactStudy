0eec279c90436ff173b2cedb834a2f399a47fec9
youngdon jung <youngdon.jung@lotte.net>
2026-04-01 09:55:30 +0900

    Set init Component

---
+++ react-native/real-app/screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { TextInput } from 'react-native';
+
+function StartGameScreen() {
+  return (
+    <View>
+      <TextInput />
+      BUTTONS
+    </View>
+  );
+}
+
+export default StartGameScreen;
