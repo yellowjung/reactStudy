4a956cc52db9b8ffcc5496d99f28802ecfe7ad03
youngdon jung <youngdon.jung@lotte.net>
2026-04-01 09:44:14 +0900

    Init new study

--- react-native/basic-app/components/GoalInput.js
+++ react-native/basic-app/components/GoalInput.js
@@ -1,8 +1,15 @@
-import { useState } from "react";
-import { StyleSheet, View, TextInput, Button, Modal, Image } from "react-native";
+import { useState } from 'react';
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Button,
+  Modal,
+  Image,
+} from 'react-native';
 
 function GoalInput(props) {
-  const [enteredGoalText, setEnteredGoalText] = useState("");
+  const [enteredGoalText, setEnteredGoalText] = useState('');
 
   function goalInputHandler(enteredText) {
     setEnteredGoalText(enteredText);
@@ -10,13 +17,16 @@ function GoalInput(props) {
 
   function addGoalHandler() {
     props.onAddGoal(enteredGoalText);
-    setEnteredGoalText("");
+    setEnteredGoalText('');
   }
 
   return (
     <Modal visible={props.visible} animationType="slide">
       <View style={styles.inputContainer}>
-        <Image style={styles.image} source={require('../assets/images/goal.png')}/>
+        <Image
+          style={styles.image}
+          source={require('../assets/images/goal.png')}
+        />
         <TextInput
           style={styles.textInput}
           placeholder="Your course goal!"
@@ -25,10 +35,10 @@ function GoalInput(props) {
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
-            <Button title="Cancel" onPress={props.onCancel} color="#f31282"/>
+            <Button title="Cancel" onPress={props.onCancel} color="#f31282" />
           </View>
           <View style={styles.button}>
-            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0"/>
+            <Button title="Add Goal" onPress={addGoalHandler} color="#b180f0" />
           </View>
         </View>
       </View>
@@ -41,31 +51,31 @@ export default GoalInput;
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
+    justifyContent: 'center',
+    alignItems: 'center',
     padding: 16,
-    backgroundColor: '#311b6b'
+    backgroundColor: '#311b6b',
   },
   image: {
     width: 100,
     height: 100,
-    margin:20
+    margin: 20,
   },
   textInput: {
     borderWidth: 1,
-    borderColor: "#e4d0ff",
-    backgroundColor: "#e4d0ff",
+    borderColor: '#e4d0ff',
+    backgroundColor: '#e4d0ff',
     color: '#120438',
     borderRadius: 6,
-    width: "100%",
+    width: '100%',
     padding: 16,
   },
   buttonContainer: {
     marginTop: 16,
-    flexDirection: 'row'
+    flexDirection: 'row',
   },
   button: {
     width: 100,
-    marginHorizontal: 8
-  }
+    marginHorizontal: 8,
+  },
 });
