ca1d0b85ff3827d44d99a59189f0f81347c4179b
youngdon jung <youngdon.jung@lotte.net>
2026-04-07 09:11:49 +0900

    [Sec4] dsgisn: Button feedback

--- react-native/game-practice/screens/StartGameScreen.js
+++ react-native/game-practice/screens/StartGameScreen.js
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -18,7 +24,7 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#72063c",
+    backgroundColor: "#4e0329",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
@@ -30,7 +36,7 @@ const styles = StyleSheet.create({
   numberInput: {
     height: 50,
     width: 50,
-    fontSize: 32,
+    fontSize: 27,
     borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
     color: "#ddb52f",
