2a392a17722ac3d562113be939e4b5e8b13e6dbd
youngdon jung <youngdon.jung@lotte.net>
2026-04-08 09:20:31 +0900

    [Sec4] Button functions

--- react-native/game-practice/App.js
+++ react-native/game-practice/App.js
@@ -11,7 +11,7 @@ export default function App() {
         style={styles.rootScreen}
         imageStyle={styles.backgroundImage}
       >
-        <StartGameScreen />;
+        <StartGameScreen />
       </ImageBackground>
     </LinearGradient>
   );
