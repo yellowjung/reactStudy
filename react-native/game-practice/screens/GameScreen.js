5fa4a1691077b1570593c5de27e8dc8670abd5f2
youngdon <yeavov@gmail.com>
2026-04-25 23:52:27 +0900

    [Sec4] Use FlatList

--- react-native/game-practice/screens/GameScreen.js
+++ react-native/game-practice/screens/GameScreen.js
@@ -85,6 +85,11 @@ function GameScreen({ userNumber, onGameOver }) {
         {/* {guessRounds.map((guessRounds) => (
           <Text key={guessRounds}>{guessRounds}</Text>
         ))} */}
+        <FlatList
+          data={guessRounds}
+          renderItem={(itemData) => <Text>{itemData.item}</Text>}
+          keyExtractor={(item) => item}
+        />
       </View>
     </View>
   );
