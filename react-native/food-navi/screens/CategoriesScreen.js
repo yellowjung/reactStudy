5fca8b510df5efb7094174a91e29df88900131f2
youngdon jung <youngdon.jung@lotte.net>
2026-05-07 10:20:16 +0900

    [Sec5] Print food category

--- react-native/food-navi/screens/CategoriesScreen.js
+++ react-native/food-navi/screens/CategoriesScreen.js
@@ -1,13 +1,24 @@
-import { FlatList } from "react-native";
-import { CATEGORIES } from "../data/dummy-data";
+import { FlatList } from 'react-native';
+import { CATEGORIES } from '../data/dummy-data';
+import CategoryGridTitle from '../components/CategoryGridTitle';
 
-function renderCategoryItem(item){
-    return 
+function renderCategoryItem(itemData) {
+  return (
+    <CategoryGridTitle
+      title={itemData.item.title}
+      color={itemData.item.color}
+    />
+  );
 }
 
 function CategoriesScreen() {
-  return <FlatList data={CATEGORIES} keyExtractor={(item) => item.id} 
-  renderItem={}/>;
+  return (
+    <FlatList
+      data={CATEGORIES}
+      keyExtractor={(item) => item.id}
+      renderItem={renderCategoryItem}
+    />
+  );
 }
 
 export default CategoriesScreen;
