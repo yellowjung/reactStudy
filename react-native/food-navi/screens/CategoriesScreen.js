5fa613ebe759f4a1666a037282b19d9c51ffa3de
youngdon jung <youngdon.jung@lotte.net>
2026-05-15 09:51:22 +0900

    [Sec5] React Navigation property

--- react-native/food-navi/screens/CategoriesScreen.js
+++ react-native/food-navi/screens/CategoriesScreen.js
@@ -5,7 +5,9 @@ import CategoryGridTitle from '../components/CategoryGridTitle';
 function CategoriesScreen({ navigation }) {
   function renderCategoryItem(itemData) {
     function pressHandler() {
-      navigation.navigate('MealsOverview');
+      navigation.navigate('MealsOverview', {
+        categoryId: itemData.item.id
+      });
     }
 
     return (
