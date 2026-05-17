afcb6f9c18c3d548f2b26b9eca6ef7351e3a1647
youngdon <yeavov@gmail.com>
2026-05-17 14:35:55 +0900

    [Sec5] View MealItem

--- react-native/food-navi/screens/MealsOverviewScreen.js
+++ react-native/food-navi/screens/MealsOverviewScreen.js
@@ -1,13 +1,36 @@
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, FlatList } from 'react-native';
 
 import { MEALS } from '../data/dummy-data';
+import MealItem from '../components/MealItem';
 
 function MealsOverviewScreen({ route }) {
   const catId = route.params.categoryId;
 
+  const displayedMeals = MEALS.filter((mealItem) => {
+    return mealItem.categoryIds.indexOf(catId) >= 0;
+  });
+
+  function renderMealItem(itemData) {
+    const item = itemData.item;
+
+    const mealItemProps = {
+      title: item.title,
+      imageUrl: item.imageUrl,
+      duration: item.duration,
+      complexity: item.complexity,
+      affordability: item.affordability
+    };
+
+    return <MealItem {...mealItemProps} />;
+  }
+
   return (
     <View>
-      <Text>Meals Overview Screen - {catId}</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
