cd4ebba526e72bc621152bb0ba45a2815fd98f4b
youngdon jung <youngdon.jung@lotte.net>
2026-05-18 10:22:31 +0900

    [Sec5] Food Detail view finish

--- react-native/food-navi/components/MealItem.js
+++ react-native/food-navi/components/MealItem.js
@@ -8,6 +8,7 @@ import {
 } from 'react-native';
 
 import { useNavigation } from '@react-navigation/native';
+import MealDetails from './MealDetails';
 
 function MealItem({
   id,
@@ -40,11 +41,11 @@ function MealItem({
             <Image source={{ uri: imageUrl }} style={styles.image} />
             <Text style={styles.title}>{title}</Text>
           </View>
-          <View style={styles.details}>
-            <Text style={styles.detailItem}>{duration} min</Text>
-            <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
-            <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
-          </View>
+          <MealDetails
+            duration={duration}
+            affordability={affordability}
+            complexity={complexity}
+          />
         </View>
       </Pressable>
     </View>
@@ -79,16 +80,6 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     fontSize: 18,
     margin: 8
-  },
-  details: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 8
-  },
-  detailItem: {
-    marginHorizontal: 4,
-    fontSize: 12
   },
   buttonPressed: {
     opacity: 0.5
