19e1ee5481eb160d8caeba1749849caab7eb1cec
youngdon jung <youngdon.jung@lotte.net>
2026-05-19 09:50:32 +0900

    [Sec5] Add Header

--- react-native/food-navi/App.js
+++ react-native/food-navi/App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import CategoriesScreen from './screens/CategoriesScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
