64c4800ffa1f77173e7f3254b47f8c87f6a1a528
youngdon <yeavov@gmail.com>
2026-05-25 00:41:00 +0900

    [Sec5] DrawerNavigator

--- react-native/food-navi/App.js
+++ react-native/food-navi/App.js
@@ -1,12 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import CategoriesScreen from './screens/CategoriesScreen';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+
+import CategoriesScreen from './screens/CategoriesScreen';
 import MealsOverviewScreen from './screens/MealsOverviewScreen';
 import MealDetailScreen from './screens/MealDetailScreen';
+import FavoritesScreen from './screens/FavoritesScreen';
 
 const Stack = createNativeStackNavigator();
+const Drawer = createDrawerNavigator();
+
+function DrawerNavigator() {
+  return (
+    <Drawer.Navigator
+      screenOptions={{
+        headerStyle: { backgroundColor: '#351401' },
+        headerTintColor: 'white',
+        sceneContainerStyle: { backgroundColor: '#3f2f25' }
+      }}
+    >
+      <Drawer.Screen
+        name='Categories'
+        component={CategoriesScreen}
+        options={{ title: 'All Categories' }}
+      />
+      <Drawer.Screen name='Favorites' component={FavoritesScreen} />
+    </Drawer.Navigator>
+  );
+}
 
 export default function App() {
   return (
@@ -21,23 +44,18 @@ export default function App() {
           }}
         >
           <Stack.Screen
-            name='MealsCategories'
-            component={CategoriesScreen}
+            name='Drawer'
+            component={DrawerNavigator}
             options={{
-              title: 'All Meals Categories'
+              headerShown: false
             }}
           />
+          <Stack.Screen name='MealsOverview' component={MealsOverviewScreen} />
           <Stack.Screen
-            name='MealsOverview'
-            component={MealsOverviewScreen}
-            // options={({ route, navigation }) => {
-            //   const catId = route.params.categoryId;
-            //   return {
-            //     title: catId
-            //   };
-            // }}
+            name='MealDetail'
+            component={MealDetailScreen}
+            options={{ title: 'About the Meal' }}
           />
-          <Stack.Screen name='MealDetail' component={MealDetailScreen} />
         </Stack.Navigator>
       </NavigationContainer>
     </>
