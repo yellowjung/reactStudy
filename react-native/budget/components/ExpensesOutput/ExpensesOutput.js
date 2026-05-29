22b45abaf65b539c9af6008c7891dbbd51875ec9
youngdon jung <youngdon.jung@lotte.net>
2026-05-29 09:49:24 +0900

    [Sec8] Start Expenses component

---
+++ react-native/budget/components/ExpensesOutput/ExpensesOutput.js
@@ -0,0 +1,14 @@
+import { View } from "react-native";
+import ExpensesSummary from "./ExpensesSummary";
+import ExpensesList from "./ExpensesList";
+
+function ExpensesOutput({ expenses }) {
+  return (
+    <View>
+      <ExpensesSummary />
+      <ExpensesList />
+    </View>
+  );
+}
+
+export default ExpensesOutput;
