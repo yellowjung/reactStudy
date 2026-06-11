c2c4eeb445df1634225dde70c22a14dcac1aa4ab
youngdon jung <youngdon.jung@lotte.net>
2026-06-11 10:21:42 +0900

    [Sec9] Configure Input

--- react-native/budget/screens/ManageExpense.js
+++ react-native/budget/screens/ManageExpense.js
@@ -1,9 +1,9 @@
 import { useContext, useLayoutEffect } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { View, StyleSheet, TextInput } from "react-native";
 import IconButton from "../components/UI/IconButton";
-import { GlobalStyles } from "../constants/styles";
+import ExpenseForm from "../components/ManageExpense/ExpenseForm";
 import Button from "../components/UI/Button";
-
+import { GlobalStyles } from "../constants/styles";
 import { ExpensesContext } from "../store/expenses-context";
 
 function ManageExpense({ route, navigation }) {
@@ -47,6 +47,7 @@ function ManageExpense({ route, navigation }) {
 
   return (
     <View style={styles.container}>
+      <ExpenseForm />
       <View style={styles.buttons}>
         <Button mode="flat" onPress={cancelHandler}>
           Cancel
