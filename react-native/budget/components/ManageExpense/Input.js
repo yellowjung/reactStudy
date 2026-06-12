d28186b25fc52ec2945cae9e84807810a0b5da2a
youngdon jung <youngdon.jung@lotte.net>
2026-06-12 09:47:15 +0900

    [Sec9] Start style

--- react-native/budget/components/ManageExpense/Input.js
+++ react-native/budget/components/ManageExpense/Input.js
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, TextInput, View } from "react-native";
 
+import { GlobalStyles } from "../../constants/styles";
+
 function Input({ label, textInputConfig }) {
   return (
     <View>
@@ -11,4 +13,17 @@ function Input({ label, textInputConfig }) {
 
 export default Input;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  inputContainer: {
+    marginHorizontal: 4,
+    marginVertical: 16,
+  },
+  label: {
+    fontSize: 12,
+    color: GlobalStyles.colors.primary100,
+    marginBottom: 4,
+  },
+  input: {
+    backgroundColor: GlobalStyles.colors.primary100,
+  },
+});
